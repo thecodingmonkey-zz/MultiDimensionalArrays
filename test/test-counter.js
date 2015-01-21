7fbc0914cf9e5567f411a3778e125a33a6757c45
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-21 11:10:13 -1000

    add test-counter.js case

--- test/test-counter.js
+++ test/test-counter.js
@@ -1,1 +1,12 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+ 
+var mda = require('../MultiDimensionalArray.js');
 
+describe("MultiDimensionalArray.count", function() {
+  it("should return number of elements in array that evaluate to true", function() {
+    var sample_array = [1,0,0,1,0,0,0,0,1];
+    expect(mda.count(sample_array)).to.equal(3);
+  });
+});
