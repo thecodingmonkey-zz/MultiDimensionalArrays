771610178244da3e7b7cc8b304dad004194d7397
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-21 15:32:48 -1000

    remove 1d and 3d test cases

--- test/test-generator.js
+++ test/test-generator.js
@@ -9,32 +9,6 @@ var isZeroOrOne = function(x) {
   return (x === 0 || x === 1);
 }
 
-describe('generate_1d', function() {
-  it("should return an array(length 3) with no arguments with random boolean values", function() {
-    var result = mda.generate_1d();
-    expect(result).to.have.length(3);
-
-    result.forEach(function(val) {
-      expect(val).to.satisfy(isZeroOrOne);
-    })
-  });
-
-  it("should return an array(4) with random boolean values when passed 4 as an argument", function() {
-    var result = mda.generate_1d(4);
-    expect(result).to.have.length(4);
-
-    result.forEach(function(val) {
-      expect(val).to.satisfy(isZeroOrOne);
-    });
-  });
-
-  it("should return an empty array when passed 'potato'", function() {
-    var result = mda.generate_1d('potato');
-    expect(result).to.have.length(0);
-  });
-
-});
-
 describe("generate_2d", function() {
   it("should return a 3x3 array when passed no parameters", function() {
     var result = mda.generate_2d();
@@ -76,21 +50,3 @@ describe("generate_2d", function() {
     });
   });  
 });
-
-describe("generate_3d", function() {
-  it("should generate a 3-dimensional array when passed three parameters", function() {
-    var result = mda.generate_3d(5,4,3);
-    expect(result).to.have.length(5);
-
-    result.forEach(function(outerArray) {
-      expect(outerArray).to.have.length(4);
-      outerArray.forEach(function(innerArray) {
-        expect(innerArray).to.have.length(3);
-        innerArray.forEach(function(element) {
-          expect(element).to.satisfy(isZeroOrOne);
-        });
-      });
-    });
-  });
-});
-
