5811bd2cb1dd8f734a8015aa64fe88fe80346c66
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-22 11:51:48 -1000

    add test cases for counter

--- test/test-counter.js
+++ test/test-counter.js
@@ -7,10 +7,33 @@ var should = chai.should();
 
 var mda = require('../MultiDimensionalArray.js');
  
-describe('something', function() {
-  it('should do something', function() {
-    var something = "some value";
-    expect(something).to.be.a('string');
-    something.should.equal('some value');
+describe('MultiDimensionalArray', function() {
+  it('should return the count for boolean values, val=true', function() {
+    var sample_array = [1,0,0,1,0,0,0,0,1]; 
+    expect(mda.count(sample_array, true)).to.equal(3);
+  });
+  it('should return the count for boolean values, val=false', function() {
+    var sample_array = [[1,0],[0,1],[0,0],[0,0]];
+    expect(mda.count(sample_array, false)).to.equal(6);
+  });
+  it("should return the count for non-boolean values", function() {
+    var sample_array = [  
+      [  
+        ["red","green","blue"],  
+        ["red","green","blue"],  
+        ["red","green","blue"],  
+      ],  
+      [  
+        ["red","green","red"],  
+        ["red","green","red"],  
+        ["red","green","red"],  
+      ],  
+      [  
+        ["red","red","blue"],  
+        ["red","red","blue"],  
+        ["red","red","blue"],  
+      ],  
+    ];  
+    expect(mda.count(sample_array, 'red')).to.equal(15);
   });
 });
