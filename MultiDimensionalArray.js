786ad9afa78800dee85665a73940d23d63263d1b
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-22 11:44:23 -1000

    implement generate function

--- MultiDimensionalArray.js
+++ MultiDimensionalArray.js
@@ -1,5 +1,88 @@
 module.exports = {
-  generate: function() {
-    return [[0,0,0],[1,1,1],[1,1,0]];
-  }  
+  generate: function(tier1, tier2, tier3) {
+    if (tier1 === undefined) {
+      return this.generate_2d(3,3);
+    }
+
+    if (tier2 === undefined) {
+      return this.generate_1d(tier1);
+    }
+
+    if (tier3 === undefined) {
+      return this.generate_2d(tier1, tier2);
+    }
+
+    return this.generate_3d(tier1, tier2, tier3);
+
+  },
+
+  generate_1d: function(tier1) {
+    if (tier1 === undefined) {  // default parameters
+      tier1 = default_array_size;
+    }
+
+    var result = [];
+    for(var i=0; i < tier1; i++) {
+      // val = a random value, either 0 or 1.  did NOT use a ternary here
+      var val = 0;
+      if (Math.random() > 0.5) {
+        val = 1;
+      }
+
+      result.push(val);
+    }
+    return result;
+  },
+
+  generate_2d: function(tier1, tier2) {
+    // supply default parameters
+    if (tier1 === undefined) {
+      tier1 = default_array_size;
+    }
+    if (tier2 === undefined) {
+      tier2 = default_array_size;
+    }
+
+    var result = [];
+    for(var i=0; i < tier1; i++) {
+      // process is similar to generate_1d, just generates a 1d array
+      // for each element instead of a single 1/0 element
+      var val = this.generate_1d(tier2);
+
+      result.push(val);
+    }
+    return result;
+  },
+
+  generate_3d: function(tier1, tier2, tier3) {
+    var result = [];
+    // process is similar to the 2d array loop
+    for(var i=0; i < tier1; i++) {
+      var val = this.generate_2d(tier2, tier3);
+      result.push(val);
+    }
+
+    return result;
+  },
+
+  count: function(array) {
+
+    var countTruthy = function(val) {
+      // find # of truthy values if val is an array
+      if (Array.isArray(val)) {
+        return val.reduce( function(prev, next) {
+          return prev + countTruthy(next);
+        }, 0 );
+      }
+
+      // return a number based on the truthy value of the argument
+      // if it is not an array
+      if (val) {
+        return 1;
+      }
+      return 0;
+    }
+
+    return countTruthy(array);
+  }
 }
--- test/test-generator.js
+++ test/test-generator.js
@@ -7,6 +7,10 @@ var should = chai.should();
 
 var mda = require('../MultiDimensionalArray.js');
  
+var isZeroOrOne = function(x) {
+  return (x === 0 || x === 1);
+}
+
 describe('generate', function() {
   it('should be a function', function() {
     expect(mda.generate).to.be.a('function');
@@ -25,10 +29,6 @@ describe('generate', function() {
 
     expect(result)
   });
-
-  var isZeroOrOne = function(val) {
-    return (val === 0 || val === 1)
-  }
 
   it('should consist of elements that are all 0\'s or 1\'s', function() {
     var result = mda.generate();
@@ -50,5 +50,54 @@ describe('generate', function() {
         expect(result[i]).to.satisfy(isZeroOrOne);
     }
 
+  });
+});
+
+describe('generate, 1d cases', function() {
+  it("should return an array(4) with random boolean values when passed 4 as an argument", function() {
+    var result = mda.generate(4);
+    expect(result).to.have.length(4);
+
+    result.forEach(function(val) {
+      expect(val).to.satisfy(isZeroOrOne);
+    });
+  });
+
+  it("should return an empty array when passed 'potato'", function() {
+    var result = mda.generate('potato');
+    expect(result).to.have.length(0);
+  });
+
+});
+
+describe("generate, 2d cases", function() {
+  it("should return a 5x4 when passed two parameters (5,4)", function() {
+    var result = mda.generate(5,4);
+    expect(result).to.have.length(5);
+
+    result.forEach(function(subArray) {
+      expect(subArray).to.have.length(4);
+
+      subArray.forEach(function(element) {
+        expect(element).to.satisfy(isZeroOrOne);
+      });
+    });
+  });  
+});
+
+describe("generate, 3d cases", function() {
+  it("should generate a 3-dimensional array when passed three parameters", function() {
+    var result = mda.generate_3d(5,4,3);
+    expect(result).to.have.length(5);
+
+    result.forEach(function(outerArray) {
+      expect(outerArray).to.have.length(4);
+      outerArray.forEach(function(innerArray) {
+        expect(innerArray).to.have.length(3);
+        innerArray.forEach(function(element) {
+          expect(element).to.satisfy(isZeroOrOne);
+        });
+      });
+    });
   });
 });
